277cd04e7724f6ce06f087d202f3b55afd4eb405
QUOCNGOC2004 <nguyenngocvt04@gmail.com>
2025-05-14 23:35:04 +0700

    upup

--- webkhambenh/src/App.js
+++ webkhambenh/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
 import Home from './views/js/home'; 
 import ThanhToan from './views/js/ThanhToan';
 import DatLich from './views/js/DatLich';
@@ -6,10 +6,38 @@ import DanhSachBs from './views/js/DanhSachBs';
 import QuanLyLich from './views/js/QuanLyLich';
 import NotFound from './views/js/not-found';
 import Auth from './views/js/Auth';
+import Navbar8 from './components/js/forHome/navbar8';
+import { Fragment } from 'react';
 
-function App() {
+// 👇 Tạo wrapper để dùng useLocation
+function AppContent() {
+  const location = useLocation();
+
+  // Ẩn Navbar ở các path nhất định
+  const hiddenNavbarPaths = ['/dang-nhap-dang-ky'];
+  const hideNavbar = hiddenNavbarPaths.includes(location.pathname);
+
   return (
-    <Router>
+    <>
+      {!hideNavbar && (
+        <Navbar8
+          moTaQuanLyLichHen={<span className="home-text100">Quản lý lịch hẹn của bạn</span>}
+          nguoiDung={<span className="home-text101">Người dùng</span>}
+          tinTuc={<span className="home-text102">Tin tức</span>}
+          tieuDeThanhToan={<span className="home-text103">Thanh toán</span>}
+          trangChu={<span className="home-text104">Trang chủ</span>}
+          tieuDeQuanLyLichHen={<span className="home-text105">Quản lý lịch hẹn</span>}
+          tieuDeDatLich={<span className="home-text106">Đặt lịch hẹn</span>}
+          them={<span className="home-text107">Thêm</span>}
+          moTaThanhToan={<span className="home-text108">Thanh toán trước tiền hẹn khám</span>}
+          moTaDatLich={<span className="home-text109">Đặt lịch khám trước với bác sĩ</span>}
+          gioiThieu={<span className="home-text110">Giới thiệu</span>}
+          tieuDeDanhSachBacSi={<span className="home-text111">Danh sách bác sĩ</span>}
+          moTaDanhSachBacSi={<span className="home-text112">Xem danh sách bác sĩ</span>}
+          dangNhapDangKy={<span className="home-text113">Đăng nhập/Đăng ký</span>}
+        />
+      )}
+
       <Switch>
         <Route exact path="/" component={Home} /> 
         <Route path="/thanh-toan" component={ThanhToan} />
@@ -19,6 +47,14 @@ function App() {
         <Route path="/dang-nhap-dang-ky" component={Auth} />
         <Route path="*" component={NotFound} />
       </Switch>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
     </Router>
   );
 }
