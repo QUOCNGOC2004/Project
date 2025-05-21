d75a656ad058b84bab75c1d7b26145caef2a32e5
QUOCNGOC2004 <nguyenngocvt04@gmail.com>
2025-05-22 00:57:27 +0700

    fix csdl

--- webkhambenh.sql
+++ webkhambenh.sql
@@ -32,10 +32,10 @@ CREATE TABLE doctors (
 
 -- Đổ dữ liệu vào bảng doctors
 INSERT INTO doctors (name, email, phone, co_so_kham, chuyen_khoa, mo_ta_chuc_vu, hoc_vi, kinh_nghiem, link_anh, chuc_vu) VALUES
-('PGS.TS. BS Nguyễn Thanh Hồi', 'nguyenthanhh@example.com', '0901234567', 'Bệnh viện Bạch Mai', 'Tim mạch', 'Tổng Giám đốc Bệnh viện', 'Phó Giáo sư, Tiến sĩ', 20, 'https://cdn.phenikaamec.com/phenikaa-mec/image/5-14-2025/d09837dc-0ab8-400e-b486-fe7027180151-image.webp', 'Tổng giám đốc'),
-('GS.TS. BS. Đỗ Quyết', 'doquyet@example.com', '0987654321', 'Bệnh viện Bạch Mai', 'Nội tổng hợp', 'Phó Tổng Giám Đốc Bệnh Viện', 'Giáo sư, Tiến sĩ', 25, 'https://cdn.phenikaamec.com/phenikaa-mec/image/5-14-2025/15362c14-c585-472a-9999-aacdd5919507-image.webp', 'Phó tổng giám đốc'),
-('PGS.TS. BSNT Vũ Hồng Thăng', 'vuhongthang@example.com', '0912345678', 'Bệnh viện Bạch Mai', 'Ngoại tổng hợp', 'Phó Tổng Giám Đốc Bệnh Viện', 'Phó Giáo sư, Tiến sĩ', 15, 'https://cdn.phenikaamec.com/phenikaa-mec/image/5-14-2025/b17b466f-9ff2-4569-929f-cee64024d7d3-image.webp', 'Phó tổng giám đốc'),
-('TS. BS Nguyễn Văn A', 'nguyenvana@example.com', '0923456789', 'Bệnh viện Bạch Mai', 'Tim mạch', 'Trưởng khoa Tim mạch', 'Tiến sĩ', 15, '/placeholder.svg', 'Trưởng Khoa'),
-('ThS. BS Trần Thị B', 'tranthib@example.com', '0934567890', 'Bệnh viện Bạch Mai', 'Nội tổng hợp', 'Phó khoa Nội tổng hợp', 'Thạc sĩ', 10, '/placeholder.svg', 'Phó Trưởng Khoa'),
-('BS. Nguyễn Văn C', 'nguyenvanc@example.com', '0945678901', 'Bệnh viện Bạch Mai', 'Ngoại tổng hợp', 'Bác sĩ điều trị', 'Bác sĩ', 8, '/placeholder.svg', 'Bác sĩ điều trị'),
-('BS. Nguyễn Văn D', 'nguyenvanD@example.com', '0945678901', 'Bệnh viện Bạch Mai', 'Ngoại tổng hợp', 'Bác sĩ điều trị', 'Bác sĩ', 8, '/placeholder.svg', 'Bác sĩ điều trị');
+('PGS.TS. BS Nguyễn Thanh Hồi', 'nguyenthanhh@example.com', '0901234567', 'Bệnh viện Đại học Phenikaa', 'Tim mạch', 'Tổng Giám đốc Bệnh viện', 'Tiến sĩ', 20, 'https://cdn.phenikaamec.com/phenikaa-mec/image/5-14-2025/d09837dc-0ab8-400e-b486-fe7027180151-image.webp', 'Tổng giám đốc'),
+('GS.TS. BS. Đỗ Quyết', 'doquyet@example.com', '0987654321', 'Phòng khám Đa khoa Đại học Phenikaa', 'Nội tổng hợp', 'Phó Tổng Giám Đốc Bệnh Viện', 'Tiến sĩ', 25, 'https://cdn.phenikaamec.com/phenikaa-mec/image/5-14-2025/15362c14-c585-472a-9999-aacdd5919507-image.webp', 'Phó tổng giám đốc'),
+('PGS.TS. BSNT Vũ Hồng Thăng', 'vuhongthang@example.com', '0912345678', 'Bệnh viện Đại học Phenikaa', 'Ngoại tổng hợp', 'Phó Tổng Giám Đốc Bệnh Viện', 'Tiến sĩ', 15, 'https://cdn.phenikaamec.com/phenikaa-mec/image/5-14-2025/b17b466f-9ff2-4569-929f-cee64024d7d3-image.webp', 'Phó tổng giám đốc'),
+('TS. BS Nguyễn Văn A', 'nguyenvana@example.com', '0923456789', 'Bệnh viện Đại học Phenikaa', 'Tim mạch', 'Trưởng khoa Tim mạch', 'Tiến sĩ', 15, '/placeholder.svg', 'Trưởng Khoa'),
+('ThS. BS Trần Thị B', 'tranthib@example.com', '0934567890', 'Phòng khám Đa khoa Đại học Phenikaa', 'Nội tổng hợp', 'Phó khoa Nội tổng hợp', 'Thạc sĩ', 10, '/placeholder.svg', 'Phó Trưởng Khoa'),
+('BS. Nguyễn Văn C', 'nguyenvanc@example.com', '0945678901', 'Sinh viên tòa A8', 'Ngoại tổng hợp', 'Bác sĩ điều trị', 'Bác sĩ', 8, '/placeholder.svg', 'Bác sĩ điều trị'),
+('BS. Nguyễn Văn D', 'nguyenvanD@example.com', '0945678901', 'Sinh viên tòa A8', 'Ngoại tổng hợp', 'Bác sĩ điều trị', 'Bác sĩ', 8, '/placeholder.svg', 'Bác sĩ điều trị');
